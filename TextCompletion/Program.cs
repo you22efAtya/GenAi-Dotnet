514bf66825a92847d4f33a2b2f184d59f1c7a57c
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2026-03-10 05:56:13 +0200

    Integrating gpt-4o-mini in my .net aplication

--- TextCompletion/Program.cs
+++ TextCompletion/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using System.ClientModel;
+
+//get credentials from user secrets
+IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var credentials = new ApiKeyCredential(config["GitHubModels:Token"] ?? throw new InvalidOperationException()) ;
+
+var options = new OpenAIClientOptions
+{
+    Endpoint = new Uri("https://models.github.ai/inference"),
+};
+
+//create chat client
+
+IChatClient client = new OpenAIClient(credentials, options).GetChatClient("gpt-4o-mini").AsIChatClient();
+
+//send a message to the model and get a response
+ChatResponse response = await client.GetResponseAsync("What is Ai ? explain max 20 words");
+
+Console.WriteLine(response);
