b0be63d062b8dc4188f8dd86e68232ddb31af1dc
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2026-03-13 08:32:42 +0200

    Create Query Embedding and Perform Vector Search

--- VectorSearch/Program.cs
+++ VectorSearch/Program.cs
@@ -35,3 +35,24 @@ foreach (var movie in MovieData.Movies)
     // add the overall movie to the in-memory vector store's movie collection
     await moviesStore.UpsertAsync(movie);
 }
+
+//1-Embed the user’s query
+//2-Vectorized search
+//3-Returns the records
+
+// generate the embedding vector for the user's prompt
+//var query = "I want to see family friendly movie";
+var query = "A science fiction movie about space travel";
+var queryEmbedding = await generator.GenerateVectorAsync(query);
+
+// search the knowledge store based on the user's prompt
+var searchResults = moviesStore.SearchAsync(queryEmbedding, top: 2);
+
+// see the results just so we know what they look like
+await foreach (var result in searchResults)
+{
+    Console.WriteLine($"Title: {result.Record.Title}");
+    Console.WriteLine($"Description: {result.Record.Description}");
+    Console.WriteLine($"Score: {result.Score}");
+    Console.WriteLine();
+}
