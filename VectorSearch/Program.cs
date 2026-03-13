ebbfe24ddf606c6e6f57950744ea534e9f177f2d
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2026-03-13 08:24:05 +0200

    Generate Embeddings and Store in In-memory Vector Store

--- VectorSearch/Program.cs
+++ VectorSearch/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Connectors.InMemory;
+using OpenAI;
+using System.ClientModel;
+using VectorSearch;
+
+// get credentials from user secrets
+IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var credential = new ApiKeyCredential(config["GitHubModels:Token"] ?? throw new InvalidOperationException("Missing configuration: GitHubModels:Token."));
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.github.ai/inference")
+};
+
+// Create an embedding generator (text-embedding-3-small is an example)
+IEmbeddingGenerator<string, Embedding<float>> generator =
+    new OpenAIClient(credential, options)
+        .GetEmbeddingClient("openai/text-embedding-3-small")
+        .AsIEmbeddingGenerator();
+
+// Create and populate the vector store
+var vectorStore = new InMemoryVectorStore();
+
+var moviesStore = vectorStore.GetCollection<int, Movie>("movies");
+
+await moviesStore.EnsureCollectionExistsAsync();
+
+foreach (var movie in MovieData.Movies)
+{
+    // generate the embedding vector for the movie description
+    movie.Vector = await generator.GenerateVectorAsync(movie.Description);
+
+    // add the overall movie to the in-memory vector store's movie collection
+    await moviesStore.UpsertAsync(movie);
+}
