d3beb536d01d7d007499054999a27f7d716627b0
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2026-03-18 20:34:49 +0200

    Adding Qdrant Vector Database into RAG Chat App w/ .Net Aspire

---
+++ ChatApp_RAG_Qdrant/ChatApp_RAG_Qdrant.Web/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.Extensions.AI;
+using OpenAI;
+using ChatApp_RAG_Qdrant.Web.Components;
+using ChatApp_RAG_Qdrant.Web.Services;
+using ChatApp_RAG_Qdrant.Web.Services.Ingestion;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+var openai = builder.AddAzureOpenAIClient("openai");
+openai.AddChatClient("gpt-4o-mini")
+    .UseFunctionInvocation()
+    .UseOpenTelemetry(configure: c =>
+        c.EnableSensitiveData = builder.Environment.IsDevelopment());
+openai.AddEmbeddingGenerator("text-embedding-3-small");
+
+builder.AddQdrantClient("vectordb");
+builder.Services.AddQdrantVectorStore();
+builder.Services.AddQdrantCollection<Guid, IngestedChunk>(IngestedChunk.CollectionName);
+builder.Services.AddSingleton<DataIngestor>();
+builder.Services.AddSingleton<SemanticSearch>();
+builder.Services.AddKeyedSingleton("ingestion_directory", new DirectoryInfo(Path.Combine(builder.Environment.WebRootPath, "Data")));
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+
+app.UseStaticFiles();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
