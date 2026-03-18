bb0a4ca0e613af59970e8fa502c7fd5e1bbdfddf
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2026-03-18 17:20:07 +0200

    Building .NET Chat App w/ RAG Template using Ollama and all-minilm

---
+++ ChatApp_RAG_Ollama/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+using ChatApp_RAG_Ollama.Components;
+using ChatApp_RAG_Ollama.Services;
+using ChatApp_RAG_Ollama.Services.Ingestion;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+IChatClient chatClient = new OllamaApiClient(new Uri("http://localhost:11434"),
+    "llama3.2");
+IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator = new OllamaApiClient(new Uri("http://localhost:11434"),
+    "all-minilm");
+
+var vectorStorePath = Path.Combine(AppContext.BaseDirectory, "vector-store.db");
+var vectorStoreConnectionString = $"Data Source={vectorStorePath}";
+builder.Services.AddSqliteVectorStore(_ => vectorStoreConnectionString);
+builder.Services.AddSqliteCollection<string, IngestedChunk>(IngestedChunk.CollectionName, vectorStoreConnectionString);
+
+builder.Services.AddSingleton<DataIngestor>();
+builder.Services.AddSingleton<SemanticSearch>();
+builder.Services.AddKeyedSingleton("ingestion_directory", new DirectoryInfo(Path.Combine(builder.Environment.WebRootPath, "Data")));
+builder.Services.AddChatClient(chatClient).UseFunctionInvocation().UseLogging();
+builder.Services.AddEmbeddingGenerator(embeddingGenerator);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+
+app.UseStaticFiles();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ ChatApp_RAG_Ollama/Services/IngestedChunk.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.Extensions.VectorData;
+
+namespace ChatApp_RAG_Ollama.Services;
+
+public class IngestedChunk
+{
+    public const int VectorDimensions = 384; // 384 is the default vector size for the all-minilm embedding model
+    public const string VectorDistanceFunction = DistanceFunction.CosineDistance;
+    public const string CollectionName = "data-chatapp_rag_ollama-chunks";
+
+    [VectorStoreKey(StorageName = "key")]
+    [JsonPropertyName("key")]
+    public required Guid Key { get; set; }
+
+    [VectorStoreData(StorageName = "documentid")]
+    [JsonPropertyName("documentid")]
+    public required string DocumentId { get; set; }
+
+    [VectorStoreData(StorageName = "content")]
+    [JsonPropertyName("content")]
+    public required string Text { get; set; }
+
+    [VectorStoreData(StorageName = "context")]
+    [JsonPropertyName("context")]
+    public string? Context { get; set; }
+
+    [VectorStoreVector(VectorDimensions, DistanceFunction = VectorDistanceFunction, StorageName = "embedding")]
+    [JsonPropertyName("embedding")]
+    public string? Vector => Text;
+}
